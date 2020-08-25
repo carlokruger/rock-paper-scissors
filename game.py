5ca1d44e779c9e62c6c99dda75c4619949d13e4e
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-25 14:55:34 +1200

    completed Stage 3

--- game.py
+++ game.py
@@ -3,32 +3,37 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 
-human_play = input().lower()
-ai_play = random.choice(choices)
+while True:
+    human_play = input().lower()
+    ai_play = random.choice(choices)
 
-if human_play == "rock":
-    if ai_play == "rock":
-        print("There is a draw {}".format(ai_play))
-    elif ai_play == "paper":
-        print("Sorry, but the computer chose {}".format(ai_play))
-    elif ai_play == "scissors":
-        print("Well done. The computer chose {} and failed".format(ai_play))
+    if human_play == "rock":
+        if ai_play == "rock":
+            print("There is a draw {}".format(ai_play))
+        elif ai_play == "paper":
+            print("Sorry, but the computer chose {}".format(ai_play))
+        elif ai_play == "scissors":
+            print("Well done. The computer chose {} and failed".format(ai_play))
 
-elif human_play == "scissors":
-    if ai_play == "rock":
-        print("Sorry, but the computer chose {}".format(ai_play))
-    elif ai_play == "paper":
-        print("Well done. The computer chose {} and failed".format(ai_play))
-    elif ai_play == "scissors":
-        print("There is a draw {}".format(ai_play))
+    elif human_play == "scissors":
+        if ai_play == "rock":
+            print("Sorry, but the computer chose {}".format(ai_play))
+        elif ai_play == "paper":
+            print("Well done. The computer chose {} and failed".format(ai_play))
+        elif ai_play == "scissors":
+            print("There is a draw {}".format(ai_play))
 
-elif human_play == "paper":
-    if ai_play == "rock":
-        print("Well done. The computer chose {} and failed".format(ai_play))
-    elif ai_play == "paper":
-        print("There is a draw {}".format(ai_play))
-    elif ai_play == "scissors":
-        print("Sorry, but the computer chose {}".format(ai_play))
+    elif human_play == "paper":
+        if ai_play == "rock":
+            print("Well done. The computer chose {} and failed".format(ai_play))
+        elif ai_play == "paper":
+            print("There is a draw {}".format(ai_play))
+        elif ai_play == "scissors":
+            print("Sorry, but the computer chose {}".format(ai_play))
 
-else:
-    print("Please choose a valid option")
+    elif human_play == "!exit":
+        print("Bye!")
+        break
+
+    else:
+        print("Invalid input")
