1303bb9d5e6698057f49a32fefac10923ec295cc
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-25 14:34:45 +1200

    completed Stage 1

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
 # Write your code here
+
+human_play = input().lower()
+
+if human_play == "rock":
+    print("Sorry, but the computer chose paper")
+elif human_play == "scissors":
+    print("Sorry, but the computer chose rock")
+elif human_play == "paper":
+    print("Sorry, but the computer chose scissors")
+else:
+    print("Please choose a valid option")
