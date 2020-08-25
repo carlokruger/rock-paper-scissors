60ddb44377a635b8d8e9aaa53233c9bd02250e33
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-25 14:49:07 +1200

    completed Stage 2

--- game.py
+++ game.py
@@ -1,12 +1,34 @@
+import random
 # Write your code here
 
+choices = ["rock", "paper", "scissors"]
+
 human_play = input().lower()
+ai_play = random.choice(choices)
 
 if human_play == "rock":
-    print("Sorry, but the computer chose paper")
+    if ai_play == "rock":
+        print("There is a draw {}".format(ai_play))
+    elif ai_play == "paper":
+        print("Sorry, but the computer chose {}".format(ai_play))
+    elif ai_play == "scissors":
+        print("Well done. The computer chose {} and failed".format(ai_play))
+
 elif human_play == "scissors":
-    print("Sorry, but the computer chose rock")
+    if ai_play == "rock":
+        print("Sorry, but the computer chose {}".format(ai_play))
+    elif ai_play == "paper":
+        print("Well done. The computer chose {} and failed".format(ai_play))
+    elif ai_play == "scissors":
+        print("There is a draw {}".format(ai_play))
+
 elif human_play == "paper":
-    print("Sorry, but the computer chose scissors")
+    if ai_play == "rock":
+        print("Well done. The computer chose {} and failed".format(ai_play))
+    elif ai_play == "paper":
+        print("There is a draw {}".format(ai_play))
+    elif ai_play == "scissors":
+        print("Sorry, but the computer chose {}".format(ai_play))
+
 else:
     print("Please choose a valid option")
