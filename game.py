11d55112017105e3e63cdb98fc1108015ed6b5ad
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-26 15:36:00 +1200

    completed Stage 5

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 # Write your code here
 
-choices = ["rock", "paper", "scissors"]
+default = ["rock", "paper", "scissors"]
 current_score = 0
 
 player = input("Enter your name:")
@@ -19,50 +19,56 @@ for line in file_open:
 
 file_open.close()
 
+text = input("Input game:")  # rock,paper,scissors,lizard,spock
+print("Okay, let's start")
+
+if len(text) == 0:
+    game = default
+else:
+    game = text.split(",")
+
+num_winners = (len(game) - 1) / 2  # 2
+winners = []
+
 if player in ratings:
     current_score = ratings[player]
 
 while True:
-    human_play = input().lower()
-    ai_play = random.choice(choices)
+    human_play = input("Input choice:").lower()  # scissors
+    ai_play = random.choice(game)  # spock
 
-    if human_play == "rock":
-        if ai_play == "rock":
-            print("There is a draw ({})".format(ai_play))
-            current_score += 50
-        elif ai_play == "paper":
-            print("Sorry, but the computer chose {}".format(ai_play))
-        elif ai_play == "scissors":
-            print("Well done. The computer chose {} and failed".format(ai_play))
-            current_score += 100
-
-    elif human_play == "scissors":
-        if ai_play == "rock":
-            print("Sorry, but the computer chose {}".format(ai_play))
-        elif ai_play == "paper":
-            print("Well done. The computer chose {} and failed".format(ai_play))
-            current_score += 100
-        elif ai_play == "scissors":
-            print("There is a draw ({})".format(ai_play))
-            current_score += 50
-
-    elif human_play == "paper":
-        if ai_play == "rock":
-            print("Well done. The computer chose {} and failed".format(ai_play))
-            current_score += 100
-        elif ai_play == "paper":
-            print("There is a draw ({})".format(ai_play))
-            current_score += 50
-        elif ai_play == "scissors":
-            print("Sorry, but the computer chose {}".format(ai_play))
-
-
-    elif human_play == "!rating":
+    if human_play == "!rating":
         print("Your rating: ", current_score)
 
     elif human_play == "!exit":
         print("Bye!")
         break
 
-    else:
+    elif human_play not in game:
         print("Invalid input")
+
+    if human_play in game:
+        mid_point = game.index(human_play)  # 3
+
+        if mid_point > num_winners:  # True
+            winners = game[mid_point + 1:]
+            winners.extend(game[:int(mid_point - num_winners)])
+
+        elif mid_point < num_winners:
+            winners = game[mid_point + 1: int(mid_point + num_winners) + 1]
+
+        elif mid_point == num_winners:
+            winners = game[mid_point + 1:]
+
+        if human_play == ai_play:
+            print("There is a draw ({})".format(ai_play))
+            current_score += 50
+
+        elif ai_play in winners:
+            print("Sorry, but the computer chose {}".format(ai_play))
+
+        elif ai_play not in winners:
+            print("Well done. The computer chose {} and failed".format(ai_play))
+            current_score += 100
+
+
